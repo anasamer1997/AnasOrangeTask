98c7d7e764b8665e6b55b91fdc3322278564974b
anasamer1997 <anas.amer8168@gmail.com>
2023-08-20 20:41:19 +0300

    first commit

---
+++ AnasOrangTask/AnasOrangTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnasOrangTaskApp.swift
+//  AnasOrangTask
+//
+//  Created by mac on 20/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnasOrangTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
